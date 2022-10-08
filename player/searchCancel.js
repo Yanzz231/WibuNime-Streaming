a593439a892874f098cad7577e70b3b03ddef25e
Yanzz231 <75265517+Yanzz231@users.noreply.github.com>
2022-10-08 15:31:18 +0700

    Update searchCancel.js

--- player/searchCancel.js
+++ player/searchCancel.js
@@ -2,4 +2,4 @@ module.exports = (client, message, query, tracks) => {
     message.channel.send(`${client.emotes.error} - You did not provide a valid response ... Please send the command again !`).then(msg => {
         setTimeout(() => msg.delete(), client.durasi.message)
       })
-};
+}; 
